c490b11b3fc56f112fc4b7d2b4115b80b5ba1f63
David Brown <dbrown@bnl.gov>
2024-06-08 13:03:49 +0200

    one test passing, now to write some tough ones

--- x4i/exfor_units.py
+++ x4i/exfor_units.py
@@ -4,17 +4,23 @@ import csv
 import pint
 import bidict
 
-exfor_units_dict = exfor_dicts.get_exfor_dict("Data units")  # This is the NRDC generated dictionary of unit definitions for EXFOR files
+# This is the NRDC generated dictionary of unit definitions for EXFOR files
+exfor_units_dict = exfor_dicts.get_exfor_dict("Data units")  
+for obsolete in ['PART/FIS' ,'K', 'PART/MUAHR']:
+    del(exfor_units_dict[obsolete])
 del(exfor_units_dict['SEE TEXT'])  # this ain't a unit!
-exfor_pint_unit_map = bidict.bidict()  # mapping between EXFOR units and Pint units
-exfor_unit_registry = pint.UnitRegistry()
-exfor_unit_registry.load_definitions(DICTPATH + os.sep + 'exfor-pint-definitions.txt')  # our unit registry in Pint
 
+# Set up the mapping between EXFOR units and Pint units
+exfor_pint_unit_map = bidict.bidict()  
 with open(DICTPATH + os.sep + 'exfor_units.csv', mode='r') as csvfile:
     csvreader = csv.reader(csvfile)
     for irow, row in enumerate(csvreader):
         if irow == 0: 
             continue
         exfor_pint_unit_map[row[0]]=row[-1]
+
+# Set up our unit registry in Pint
+exfor_unit_registry = pint.UnitRegistry()
+exfor_unit_registry.load_definitions(DICTPATH + os.sep + 'exfor-pint-definitions.txt')  
 
 
--- x4i/test/test_exfor_units.py
+++ x4i/test/test_exfor_units.py
@@ -8,3 +8,18 @@ class TestX4Units(unittest.TestCase):
         """Routine attempts to make each of the EXFOR units"""
         for ex in exfor_units.exfor_units_dict:
             self.assertEqual(1.0 * exfor_units.exfor_unit_registry(exfor_units.exfor_pint_unit_map[ex]).magnitude, 1.0)
+
+    def test_simple_conversions(self):
+        pass 
+
+    def test_temperatures(self):
+        pass 
+
+    def test_our_defined_easy_units(self):
+        pass 
+
+    def test_our_defined_100_units(self):
+        pass 
+
+    def test_our_defined_sqrt_units(self):
+        pass 
