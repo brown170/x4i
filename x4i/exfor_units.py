c4bdaeab466720f3048fd8fc73e5ab8e14d50533
David Brown <dbrown@bnl.gov>
2024-06-08 12:57:41 +0200

    now can make units (if defs OK)

--- x4i/exfor_units.py
+++ x4i/exfor_units.py
@@ -4,12 +4,17 @@ import csv
 import pint
 import bidict
 
-exfor_units_dict = exfor_dicts.get_exfor_dict("Data units")
-exfor_pint_unit_map = bidict.bidict()
+exfor_units_dict = exfor_dicts.get_exfor_dict("Data units")  # This is the NRDC generated dictionary of unit definitions for EXFOR files
+del(exfor_units_dict['SEE TEXT'])  # this ain't a unit!
+exfor_pint_unit_map = bidict.bidict()  # mapping between EXFOR units and Pint units
 exfor_unit_registry = pint.UnitRegistry()
+exfor_unit_registry.load_definitions(DICTPATH + os.sep + 'exfor-pint-definitions.txt')  # our unit registry in Pint
 
 with open(DICTPATH + os.sep + 'exfor_units.csv', mode='r') as csvfile:
     csvreader = csv.reader(csvfile)
-    for row in csvreader:
+    for irow, row in enumerate(csvreader):
+        if irow == 0: 
+            continue
         exfor_pint_unit_map[row[0]]=row[-1]
+
 
--- x4i/test/test_exfor_units.py
+++ x4i/test/test_exfor_units.py
@@ -1,10 +1,10 @@
 import unittest
 from x4i import exfor_units
-import pint
 
 
 class TestX4Units(unittest.TestCase):
 
     def test_create_exfor_values(self):
+        """Routine attempts to make each of the EXFOR units"""
         for ex in exfor_units.exfor_units_dict:
-            pint.Unit(exfor_units.exfor_units_dict[ex])
+            self.assertEqual(1.0 * exfor_units.exfor_unit_registry(exfor_units.exfor_pint_unit_map[ex]).magnitude, 1.0)
